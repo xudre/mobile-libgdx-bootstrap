dbf24dd99e9a30bd3feb4bd6beda5d6bd588d0e6
Pedro Xudre <pedro@xudre.com>
2019-12-19 07:54:17 -0300

    First libGDX setup.

---
+++ android/src/com/thepipecat/libgdx/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.thepipecat.libgdx;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.thepipecat.libgdx.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
